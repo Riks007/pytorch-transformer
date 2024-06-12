458d6f3551e2cbdb380df756b81251d3e95e415a
Rikshanth <109074852+Riks007@users.noreply.github.com>
2024-06-12 20:38:14 +1200

    Add files via upload

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 from pathlib import Path
 
 def get_config():
-    base_path = '/content/drive/My Drive/Transformer'
+    # base_path = '/content/drive/My Drive/Transformer'
     return {
         "batch_size": 20,
         "num_epochs": 1,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
-        "datasource": base_path,
+        "datasource": 'findnitai/english-to-hinglish',
         "lang_src": "en",
         "lang_tgt": "hi_ng",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
-        "tokenizer_file": f"{base_path}/tokenizer_{{0}}.json",
-        "experiment_name": f"{base_path}/runs/tmodel"
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+        # "tokenizer_file": f"{base_path}/tokenizer_{{0}}.json",
+        # "experiment_name": f"{base_path}/runs/tmodel"
     }
 
 def get_weights_file_path(config, epoch: str):
